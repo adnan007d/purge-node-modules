c67282e583994bc5bc2986025488f4966516ef76
adnan007d <adnanmansuri54@gmail.com>
2022-08-07 17:42:37 +0530

    fix to remove contents from dir

--- src/purge/mod.rs
+++ src/purge/mod.rs
@@ -2,7 +2,7 @@ use std::fs;
 
 pub fn purge(path: &std::path::Path) -> std::io::Result<()> {
     if path.is_dir() && path.ends_with("node_modules") {
-        fs::remove_dir(path)?;
+        fs::remove_dir_all(path)?;
     } else if path.is_dir() {
         for entry in fs::read_dir(path)? {
             let entry = entry?;
